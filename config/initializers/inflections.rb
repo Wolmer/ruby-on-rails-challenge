7dad19984a5dee5e38edcf4d7f3aa03cd6e57cab
Wolmer Pinto <wolmerwap@gmail.com>
2021-02-13 13:44:07 -0300

    add plural to "campus" word

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,7 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+ActiveSupport::Inflector.inflections do |inflect|
+    # Irregulares
+    inflect.irregular "campus", "campuses"
+  end
