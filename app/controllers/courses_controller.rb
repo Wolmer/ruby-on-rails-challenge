a098a4e97eb24941bf810f9e5fee0285af0976ea
Wolmer Pinto <wolmerwap@gmail.com>
2021-02-15 19:34:30 -0300

    create a json format to response data

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,7 +3,37 @@ class CoursesController < ApplicationController
 
   # GET /courses
   def index
+    # data     = []
     @courses = Course.all
-    json_response(@courses)
+    json_response(toJson(@courses))
+  end
+
+  private
+
+  def toJson(courses)
+    data     = []
+    courses.each do |course|
+      course.campus.map() do |campus|
+        data.push({
+          course: {
+            name: course.name,
+            kind: course.kind,
+            level: course.level,
+            shift: course.shift,
+            university: {
+              name: course.university.name,
+              score: course.university.score,
+              logo_url: course.university.logo_url
+            },
+            campus: {
+              name: campus.name,
+              city: campus.city
+            }
+          }
+        })
+      end
+    end
+
+    return data
   end
 end
--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,6 +4,41 @@ class OffersController < ApplicationController
   # GET /offers
   def index
     @offers = Offer.all
-    json_response(@offers)
+    json_response(toJson(@offers))
+  end
+
+  private
+
+  def toJson(offers)
+    data     = []
+    offers.each do |offer|
+      offer.course.campus.each() do |campus|
+        data.push({
+          full_price: offer.course.price,
+          price_with_discount: offer.price_with_discount,
+          discount_percentage: offer.discount_percentage,
+          start_date: offer.start_date,
+          enrollment_semester: offer.enrollment_semester,
+          enabled: offer.enabled,
+          course: {
+            name: offer.course.name,
+            kind: offer.course.kind,
+            level: offer.course.level,
+            shift: offer.course.shift
+          },
+          university: {
+            name: offer.course.university.name,
+            score: offer.course.university.score,
+            logo_url: offer.course.university.logo_url
+          },
+          campus: {
+            name: campus.name,
+            city: campus.city
+          }
+        })
+      end
+    end
+
+    return data
   end
 end
